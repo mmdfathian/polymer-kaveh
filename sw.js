c8167624973e51406ebafd013a9ee81d0f22c2bd
mmdfathian <mmdfathian@users.noreply.github.com>
2026-08-22 16:36:18 +0000

    fix: bump SW cache to v04 (stale half-CSS), style calculator-cta

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Scope: / (GitHub Pages root)
  */
 
-const CACHE_NAME = 'polymer-kaveh-v20260822-03';
+const CACHE_NAME = 'polymer-kaveh-v20260822-04';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
